61e9649a5afe83a93e8c8517c04bde08bd82a9f6
Sahil-Mhatre14 <sahil81623@gmail.com>
2025-07-05 22:18:28 -0700

    add interface

--- lib/demoawspipeline-app-stack.ts
+++ lib/demoawspipeline-app-stack.ts
@@ -2,8 +2,12 @@ import * as cdk from 'aws-cdk-lib'
 import { Construct } from "constructs"
 import { lambdaStack } from './lambda-stack' 
 
+interface PipelineAppStageProps extends cdk.StageProps {
+  stageName: string;
+}
+
 export class PipelineAppStage extends cdk.Stage {
-  constructor(scope: Construct, id: string, props?: cdk.StageProps & { stageName: string }) {
+  constructor(scope: Construct, id: string, props: PipelineAppStageProps) {
     super(scope, id, props);
 
     new lambdaStack(this, `LambdaStack-${props.stageName}`, {
--- lib/lambda-stack.ts
+++ lib/lambda-stack.ts
@@ -2,8 +2,11 @@ import * as cdk from 'aws-cdk-lib'
 import {Construct} from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 
+interface LambdaStackProps extends cdk.StackProps {
+  stageName: string;
+}
 export class lambdaStack extends cdk.Stack {
-    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    constructor(scope: Construct, id: string, props: LambdaStackProps) {
         super(scope, id, props);
 
         const demolambda = new lambda.Function(this, `lambda-${props.stageName}`, {
